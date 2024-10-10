4b8d18c730e9fe62489accc43f0420117c32bf04
Fillhx <gustavo.restrepo@correounivalle.edu.co>
2024-10-09 21:54:37 -0500

    he hecho un pequeño avance personal, incluso la clase base llamada soldado

---
+++ Mini proyecto 1/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Ingrese su Id: ");
+        String id = scanner.nextLine();
+        System.out.println("Ingrese su Rango: ");
+        String rango = scanner.nextLine();
+        Soldado soldado1 = new Soldado(nombre, id, rango);
+        soldado1.mostrarInformacion();
+    }
+}
