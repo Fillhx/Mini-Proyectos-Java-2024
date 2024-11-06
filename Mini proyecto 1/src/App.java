c7ad31f2e6656dfb8ae9af8a4b2b295bf5e908a4
sofiaf3 <laura.rivera.arias@correounivalle.edu.co>
2024-11-05 20:12:22 -0500

    cambios en app

--- Mini proyecto 1/src/App.java
+++ Mini proyecto 1/src/App.java
@@ -1,14 +1,59 @@
 import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese su nombre: ");
         String nombre = scanner.nextLine();
         System.out.println("Ingrese su Id: ");
         String id = scanner.nextLine();
-        System.out.println("Ingrese su Rango: ");
-        String rango = scanner.nextLine();
+        boolean validacionCorrecta = false;
+        Rango rangoObjeto = null;
+
+        do{
+            System.out.println("Cual es tu rango?\nSoldado Raso\nTeniente\nCapitan\nCoronel ");
+            String rango = scanner.nextLine().toUpperCase();
+            switch (rango.toUpperCase()) {
+
+                case "SOLDADO RASO":
+                    rangoObjeto = new SoldadoRaso(1,nombre, id, rango );
+                    validacionCorrecta = true;
+                    break;
+
+                case "TENIENTE":
+                System.out.println("Ingrese la unidad del teniente: ");
+                String unidad = scanner.nextLine();
+                rangoObjeto = new Teniente(2, nombre, id, rango, unidad);
+                validacionCorrecta = true;
+                break;
+
+                case "CAPITAN":
+                System.out.println("Ingrese la cantidad de soldados bajo su mando: ");
+                int soldadosBajoSuMando = scanner.nextInt();
+                rangoObjeto = new Capitan( 3, nombre, id, rango, soldadosBajoSuMando);
+                validacionCorrecta = true;
+                break;
+
+                case "CORONEL":
+                System.out.println("Ingrese la estrategia del coronel: ");
+                String estrategia = scanner.nextLine();
+                rangoObjeto = new Coronel(4, nombre, id, rango, estrategia);
+                validacionCorrecta = true;
+                break;
+
+                default:
+                System.out.println("Ingrese un rango correcto");
+                break;
+            }
+
+        } while (!validacionCorrecta);
+        
+        if(rangoObjeto != null){
+            System.out.println("*********************************************************");
+            rangoObjeto.realizarAccion();
+        
+        }
+
         scanner.close();
     }
 }
